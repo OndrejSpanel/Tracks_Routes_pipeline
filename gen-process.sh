1dfa01539abf4466d4a9721d1f9a6f969b940c19
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2018-05-29 12:35:33 +0200

    Fix various Bash issues.

--- gen-process.sh
+++ gen-process.sh
@@ -1,20 +1,20 @@
-set prefix=$1
-set base = "..\$prefix"
-echo "Processing $pbf"
+prefix=$1
+base="../Map/$prefix"
+echo "Processing $base.osm.pbf"
 echo "Adding tags"
-./osmconvert64.exe --parameter-file=modifypars.txt "$base.osm.pbf" -o="$base.o5m"
+../bin/osmconvert64 --parameter-file=modifypars.txt "$base.osm.pbf" -o="$base.o5m"
 echo "Filtering"
 echo " routes"
-./osmfilter.exe --parameter-file=filterpars.txt "$base.o5m" -o="$base-relations.osm"
+../bin/osmfilter --parameter-file=filterpars.txt "$base.o5m" -o="$base-relations.osm"
 echo " power"
-./osmfilter.exe --parameter-file=filterpars-power.txt "$base.o5m" -o="$base-power.osm"
+../bin/osmfilter --parameter-file=filterpars-power.txt "$base.o5m" -o="$base-power.osm"
 echo " tourism"
-./osmfilter.exe --parameter-file=filterpars-bus.txt "$base.o5m" -o="$base-bus-stops.osm"
+../bin/osmfilter --parameter-file=filterpars-bus.txt "$base.o5m" -o="$base-bus-stops.osm"
 
 set OSM_CONFIG_FILE="./osmconf.ini"
 
 echo "Extracting layers"
-ogr2ogr.exe -f GeoJSON "$base-relations.geojson" "$base-relations.osm" multilinestrings
-ogr2ogr.exe -f GeoJSON "$base-relation-points.geojson" "$base-relations.osm" points
-ogr2ogr.exe -f GeoJSON "$base-power-ways.geojson" "$base-power.osm" lines
-ogr2ogr.exe -f GeoJSON "$base-bus-stops.geojson" "$base-bus-stops.osm" points
+ogr2ogr -f GeoJSON "$base-relations.geojson" "$base-relations.osm" multilinestrings
+ogr2ogr -f GeoJSON "$base-relation-points.geojson" "$base-relations.osm" points
+ogr2ogr -f GeoJSON "$base-power-ways.geojson" "$base-power.osm" lines
+ogr2ogr -f GeoJSON "$base-bus-stops.geojson" "$base-bus-stops.osm" points
