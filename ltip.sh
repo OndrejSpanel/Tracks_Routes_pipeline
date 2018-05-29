cf572c89558580252c5b764844fdc1dba0ce48b9
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2018-05-29 15:24:00 +0200

    Provide CRLF settings, improve readme.

---
+++ ltip.sh
@@ -0,0 +1,1 @@
+../tippecanoe/tippecanoe -o $1.mbtiles -f -Z8 -z12 -ao -ac -ar --drop-densest-as-needed $1.geojson
---
+++ xtip.sh
@@ -0,0 +1,3 @@
+./ltip.sh ../Map/$1-power-ways
+./ltip.sh ../Map/$1-bus-stops
+./ltip.sh ../Map/$1-relations
