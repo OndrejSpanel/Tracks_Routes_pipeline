44fe021709099580879a61cc103bc1e5e0535a9d
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2018-06-01 14:48:37 +0200

    Improve current file designation.

--- gen-tracks.sh
+++ gen-tracks.sh
@@ -1,6 +1,6 @@
 echo "Diff"
 base="../Map/$1"
-../bin/osmconvert $base-ref.o5m $base.osm.pbf --diff -o="$base-changes.o5c"
+../bin/osmconvert $base-ref.o5m $base-curr.osm.pbf --diff -o="$base-changes.o5c"
 echo "Filter tracks"
 ../bin/osmfilter --parameter-file=filterpars-tracks.txt "$base-changes.o5c" -o="$base-changes-tracks.osm"
 echo "Convert to Geojson"
