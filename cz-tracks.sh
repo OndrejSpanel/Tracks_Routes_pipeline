f0fd2a04a8ce988c6ff3770b113e543120dc51d7
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2018-07-15 18:24:48 +0200

    Track changes ref state creation.

--- cz-tracks.sh
+++
@@ -1,6 +0,0 @@
-echo "Diff"
-.\osmconvert ..\czech-republic-ref.o5m ..\czech-republic-latest.osm.pbf --diff -o="..\cz-changes.o5c"
-echo "Filter tracks"
-.\osmfilter.exe --parameter-file=filterpars-tracks.txt "..\cz-changes.o5c" -o="..\cz-changes-tracks.osm"
-echo "Convert to Geojson"
-.\bin\ogr2ogr.exe -f GeoJSON "..\cz-changes-tracks.geojson" "..\cz-changes-tracks.osm" lines
---
+++ ref-define.sh
@@ -0,0 +1,2 @@
+base="../Map/$1"
+../bin/osmconvert $2 -o="$base/ref.o5m"
