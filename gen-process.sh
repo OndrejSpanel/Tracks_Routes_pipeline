903ad7977ab00b2df53a7ddf0690cf3d8a8e8af4
Ondřej Španěl <OndrejSpanel@users.noreply.github.com>
2020-05-31 21:27:18 +0200

    Add cutline processing, improve docs

--- gen-process.sh
+++ gen-process.sh
@@ -9,6 +9,8 @@ echo " routes"
 ../bin/osmfilter --parameter-file=filterpars.txt "$base/data.o5m" -o="$base/relations.osm"
 echo " power"
 ../bin/osmfilter --parameter-file=filterpars-power.txt "$base/data.o5m" -o="$base/power.osm"
+echo " other-lines"
+../bin/osmfilter --parameter-file=filterpars-other-lines.txt "$base/data.o5m" -o="$base/other-lines.osm"
 echo " tourism"
 ../bin/osmfilter --parameter-file=filterpars-poi.txt "$base/data.o5m" -o="$base/poi.osm"
 
@@ -19,6 +21,8 @@ rm "$base/relations.geojson"
 ogr2ogr -f GeoJSON "$base/relations.geojson" "$base/relations.osm" multilinestrings
 rm "$base/power-ways.geojson"
 ogr2ogr -f GeoJSON "$base/power-ways.geojson" "$base/power.osm" lines
+rm "$base/other-lines.geojson"
+ogr2ogr -f GeoJSON "$base/other-lines.geojson" "$base/other-lines.osm" lines
 rm "$base/poi.geojson"
 ogr2ogr -f GeoJSON "$base/poi.geojson" "$base/poi.osm" points
 rm "$base/poi-poly.geojson"
--- xtip.sh
+++ xtip.sh
@@ -1,4 +1,5 @@
 base=../Map/$1
 ./ltip.sh $base/power-ways
+./ltip.sh $base/other-lines
 ./ltipx.sh $base/poi.mbtiles $base/poi.geojson $base/poi-poly.geojson $base/poi-line.geojson -l poi
 ./ltip.sh $base/relations
